65863745bbddd8c406a306f25bee5e2ef14ae1ba
alexthecurator <edge.works.curator@gmail.com>
2024-08-09 01:43:48 +0300

    Post card view functionality

--- src/app/(forum)/home/data.js
+++ src/app/(forum)/home/data.js
@@ -1,9 +1,21 @@
 export const Posts = [
   {
+    title:
+      "The Emergence of Quantum-Driven Agriculture (QDA) and Its Impact on Future Farming",
+    user: {
+      name: "Artificial Intelligence",
+      date: "08.08.2024",
+    },
+    description:
+      "In the ever-evolving landscape of agriculture, a groundbreaking concept known as Quantum-Driven Agriculture (QDA) is making waves as the next frontier in farming technology. QDA leverages quantum computing principles to optimize every aspect of agricultural production, from soil health to crop yield predictions, by analyzing vast datasets at unprecedented speeds. What is Quantum-Driven Agriculture?Quantum-Driven Agriculture refers to the application of quantum computing techniques in agriculture to solve complex problems that classical computing struggles to handle. By utilizing quantum bits (qubits) instead of traditional binary bits, QDA can process information at a scale and speed that is orders of magnitude faster than conventional methods. How Does QDA Work? QDA uses quantum algorithms to simulate and model plant growth, soil conditions, and weather patterns with extraordinary precision. These simulations allow farmers to make data-driven decisions in real-time, ensuring optimal planting schedules, efficient resource allocation, and even predicting crop diseases before they manifest.The Benefits of QDA Enhanced Crop Yields: By predicting the best conditions for planting and harvesting, QDA ensures maximum productivity. Sustainable Farming: QDA models can help reduce water usage and minimize chemical inputs, leading to more sustainable farming practices. Climate Resilience: With the ability to simulate various climate scenarios, QDA helps farmers adapt to changing weather patterns, reducing the risk of crop failure. The Future of QDA As quantum computing technology advances, so too will the capabilities of Quantum-Driven Agriculture. The integration of QDA in farming is still in its infancy, but early adopters are already seeing significant improvements in efficiency and yield. In the coming years, we can expect QDA to become a standard practice in the agricultural industry, driving the next green revolution",
+
+    conferences: ["ICLR", "EC"],
+  },
+  {
     title: "Hello, i am a post",
     user: {
       name: "Elizabeth Writer",
-      date: "",
+      date: "07.08.2024",
     },
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
@@ -14,7 +26,7 @@ export const Posts = [
     title: "Hello, i am another post",
     user: {
       name: "Elizabeth Writer",
-      date: "",
+      date: "07.08.2024",
     },
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
@@ -25,18 +37,7 @@ export const Posts = [
     title: "Hello, i am a post",
     user: {
       name: "Elizabeth Writer",
-      date: "",
-    },
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-
-    conferences: ["ICLR", "EC"],
-  },
-  {
-    title: "Hello, i am another post",
-    user: {
-      name: "Elizabeth Writer",
-      date: "",
+      date: "07.08.2024",
     },
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
--- src/app/(forum)/home/page.jsx
+++ src/app/(forum)/home/page.jsx
@@ -1,15 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
 import { Posts } from "./data";
+import { trimming } from "@/lib/utils";
 
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 import Typography from "@mui/material/Typography";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 
 let Page = () => {
+  let [readMore, setReadMore] = useState(0);
+
   return (
     <Stack spacing={3} sx={{ padding: 8 }}>
       {Posts?.map((post, index) => {
@@ -18,16 +24,33 @@ let Page = () => {
             <CardContent>
               <Stack spacing={3} sx={{ padding: 2 }}>
                 <Typography variant="h4">{post?.title}</Typography>
+                {/* Metadata */}
                 <Stack direction="row" justifyContent="space-between">
-                  <Stack direction="row" spacing={2}>
+                  <Stack direction="row" alignItems="center" spacing={2}>
                     <Typography>{post?.user?.name}</Typography>
-                    <Button>Follow</Button>
+                    <Button variant="contained" disableElevation>
+                      Follow
+                    </Button>
                   </Stack>
-                  <Typography>{post?.user?.date}</Typography>
+                  <Typography fontWeight={100}>{post?.user?.date}</Typography>
                 </Stack>
-                <Typography variant="p">{post?.description}</Typography>
+                {/* Description */}
+                <Stack spacing={1}>
+                  <Typography variant="p">
+                    {readMore && readMore === post?.title
+                      ? post?.description
+                      : trimming(post?.description, 500)}
+                  </Typography>
+                  <Button
+                    variant="text"
+                    onClick={() => setReadMore(post?.title)}
+                  >
+                    Read full post
+                  </Button>
+                </Stack>
               </Stack>
             </CardContent>
+            <CardActions>{/*  */}</CardActions>
           </Card>
         );
       })}

361e46c80489a4d844cd67d5856e6740e19b2f54
dianetc <dtchuindjo@gmail.com>
2024-09-02 16:36:48 -0400

    loading page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,6 @@
 import "./globals.css";
-
-import StoreProvider from "./provider";
-
-import SWR from "./swr";
-import Box from "@mui/material/Box";
-import MuiThemeProvider from "./theme";
-
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Script from 'next/script';
+import ClientLayout from './ClientLayout';
 
 export const metadata = {
   title: "kleesia",
@@ -18,21 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
-          strategy="beforeInteractive"
-        />
-      </head>
       <body>
-        <StoreProvider>
-          <SWR>
-            <MuiThemeProvider>
-              <ToastContainer />
-              {children}
-            </MuiThemeProvider>
-          </SWR>
-        </StoreProvider>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );

626c9a0dff7fca7bd0684b0eab582e6f2879e3d8
dianetc <dtchuindjo@gmail.com>
2024-09-02 08:16:17 -0400

    mathjax link

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <Script
-          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"
+          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
           strategy="beforeInteractive"
         />
       </head>

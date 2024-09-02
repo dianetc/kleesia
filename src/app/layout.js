975d0f7f538344dfc2fe361ecd59eb9d296c5d21
dianetc <dtchuindjo@gmail.com>
2024-09-02 16:01:57 -0400

    after interaction to ensure initial load up

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
       <head>
         <Script
           src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
-          strategy="beforeInteractive"
+          strategy="afterInteractive"
         />
       </head>
       <body>

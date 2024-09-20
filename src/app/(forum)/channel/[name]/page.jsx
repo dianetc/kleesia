35a739fa2ea802082cb98c93df4a4c8f4eac2cd2
dianetc <dtchuindjo@gmail.com>
2024-09-20 10:49:42 -0400

    channel url in address bar

--- src/app/(forum)/channel/[name]/page.jsx
+++ src/app/(forum)/channel/[name]/page.jsx
@@ -1,41 +1,44 @@
 "use client";
 
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setDetails } from '@/store/slices/data';
 import Main from '../../main';
-import { useParams } from 'next/navigation';
-import { CircularProgress, Box, Typography } from '@mui/material';
+import { usePathname } from 'next/navigation';
 import useSWR from 'swr';
 import { fetcher } from '@/lib/request';
+import { CircularProgress, Box, Typography } from '@mui/material';
 
 export default function ChannelPage() {
   const dispatch = useDispatch();
-  const params = useParams();
-  const channelName = params.name;
+  const pathname = usePathname();
+  const channelName = decodeURIComponent(pathname.split('/').pop());
 
-  const { data: channelData, error } = useSWR(`topic/get`, fetcher);
+  const { data: channelData, error, isLoading } = useSWR(`topic/get?rtf=id,title`, fetcher);
 
   useEffect(() => {
-    if (channelData) {
-      const channel = channelData.find(topic => topic.name === channelName);
-      if (channel) {
-        dispatch(setDetails({ context: "topic", id: channel.id, title: channel.name }));
+    if (channelData && Array.isArray(channelData)) {
+      const matchingChannel = channelData.find(channel => channel.title === channelName);
+      if (matchingChannel) {
+        dispatch(setDetails({ context: "topic", id: matchingChannel.id, title: matchingChannel.title }));
       }
     }
   }, [dispatch, channelName, channelData]);
 
-  if (error) {
-    return <Typography color="error">Error loading channel data</Typography>;
-  }
-
-  if (!channelData) {
+  if (isLoading) {
     return (
       <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
         <CircularProgress />
       </Box>
     );
   }
+
+  const matchingChannel = channelData.find(channel => channel.title === channelName);
+
+  if (!matchingChannel) {
+    return <Typography>Channel not found</Typography>;
+  }
+
 
   return <Main />;
 }
